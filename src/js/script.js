5c69cf45422a5569a629d052dee4deb5f97b53fa
Hlib <xlebywekmail@gmail.com>
2021-10-18 17:29:26 +0300

    minor bug fixes, added responsive for feedback section

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,9 @@
 
 $(function(){
     $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: true,   
+        speed: 1000, 
+        autoplay: true,
+        autoplaySpeed: 5000, 
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-proper.svg" alt="" width="31" height="50"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-proper.svg" alt="" width="31" height="50"></button>',
         responsive: [
