8f6d1b79baab94c340429899fae048630ba2204f
Hlib Drobinko <xlebywekmail@gmail.com>
2021-10-28 09:47:42 +0300

    minor fixes, added modal windows, modal style and  scripts for order modal

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,21 @@ $(function(){
     }
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+
+    //MODAL
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
+
