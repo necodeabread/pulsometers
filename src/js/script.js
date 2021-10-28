c84690bb7e8a0da88ab77de2d857ca4f23dd6f98
Hlib Drobinko <xlebywekmail@gmail.com>
2021-10-28 11:49:18 +0300

    added forms validation

--- src/js/script.js
+++ src/js/script.js
@@ -51,5 +51,36 @@ $(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите хотя бы {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Некорректный формат"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form')
 });
 
